88a3114bc63bd5988b6e6d1807c4fc64c2d7b6d7
tushar-wiz <tushar.u110@gmail.com>
2021-07-05 23:57:05 +0530

    "Jul 5 Revision"

--- chip8.c
+++ chip8.c
@@ -2,31 +2,33 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-static uint16_t opcodes;
+#define FONTSET_ADDRESS 0x50
+
+uint16_t opcodes;
 //program memory location begins at 512 (0x200)
 //The uppermost 256 bytes (0xF00-0xFFF)(3840-4095) are reserved for display refresh
 // 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
 // 1 byte x 4K Memory
-static uint8_t memory[4096];
+uint8_t memory[4096];
 
 // 1 byte x 16 Registers
-static uint8_t V[16];
+uint8_t V[16];
 
 // 1 byte index register
-static uint16_t I;
+uint16_t I;
 
 // 2 byte program Counter
-static uint16_t pc;
+uint16_t pc;
 
 // 1 byte x 16 gor keypad state
-static uint8_t keypad[16];
+uint8_t keypad[16];
 
 // 1 byte x 2K Video Memory
-static uint8_t gfx[64 * 32];
+uint8_t gfx[64 * 32];
 
 // stack and stack pointer (To make the emulator run not in actual chip 8)
-static uint16_t stack[16];
-static uint16_t sp;
+uint16_t stack[16];
+uint16_t sp;
 
 static uint8_t chip8_fontset[80] = {
   0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -55,9 +57,17 @@ static uint8_t soundTimer;
 
 void initialize();
 void emulateCycle();
+void Dxyn();
+void printScreen();
 
 int main(){
     initialize();
+
+    for(;;){
+        emulateCycle();
+        //system("clear");
+        printScreen();
+    }
     return 0;
 }
 
@@ -70,13 +80,16 @@ void initialize(){
     for(int i=0;i<16;i++)
         V[i] = 0;
 
+    for(int i=0;i<4096;i++)
+        memory[i] = 0;
+
     // Load fontset into memory locs
     for(int i=0;i<80;i++)
-        memory[i+80] = chip8_fontset[i];
+        memory[FONTSET_ADDRESS + i] = chip8_fontset[i];
 
     // Load Program into memory
     uint8_t* buf;
-    char fileName[] = "Pong.ch8";
+    char fileName[] = "test_opcode.ch8";
     FILE *ptr;
     ptr = fopen(fileName,"rb");
 
@@ -101,10 +114,26 @@ void emulateCycle(){
     // Fetch opcode
     opcodes = (memory[pc] << 8)|(memory[pc+1]);
 
+    uint8_t temp,val;
     // Decode opcode
 
     switch (opcodes & 0xF000){
-    case 0x0000:
+    case 0x00E0:
+        switch (opcodes & 0x000F){
+        case 0x0:
+            for(int i=0;i<(64*32);i++){
+                gfx[i] = 0x00;
+            }
+            break;
+        
+        case 0xE:
+            --sp;
+	        pc = stack[sp];
+            break;
+        
+        default:
+            break;
+        }
         break;
     
     case 0x1000:
@@ -213,6 +242,7 @@ void emulateCycle(){
         break;
     case 0xD000:
         // To be Implemented
+        Dxyn();
         break;    
     case 0xE000:
         if((opcodes & 0x00FF) == 0x9E){
@@ -224,13 +254,14 @@ void emulateCycle(){
                 pc += 2;
             }
         break;
+
     case 0xF000:
         switch (opcodes & 0x00FF){
         case 0x07:
             V[(opcodes & 0x0F00)>>8] = delayTimer;
             break;
         case 0x0A:
-            uint8_t temp = (opcodes & 0x0F00) >> 8;
+            temp = (opcodes & 0x0F00) >> 8;
             if(keypad[0]) V[temp] = 0;
             else if(keypad[1]) V[temp] = 1;
             else if(keypad[2]) V[temp] = 2;
@@ -263,13 +294,89 @@ void emulateCycle(){
             break;
         
         case 0x29:
-
+            I = FONTSET_ADDRESS + (V[(opcodes & 0x0F00) >> 8] * 5);
+            break;
+        
+        case 0x33:
+            val = V[(opcodes & 0x0F00) >> 8];
+            memory[I+2] = val%10;
+            val /= 10;
+            memory[I+1] = val%10;
+            val /= 10;
+            memory[I] = val%10;
+            break;
+        
+        case 0x55:
+            for(uint8_t i=0;i<V[(opcodes & 0x0F00) >> 8];i++)
+                memory[I + i] = V[i];
+            break;
+        
+        case 0x65:
+            for(uint8_t i=0;i<V[(opcodes & 0x0F00) >> 8];i++)
+                V[i] = memory[I + i];
+            break;            
         default:
             break;
         }
     default:
         break;
     }
-
+    pc += 2;
     // Timers
+}
+
+
+// Copied Function from internet
+void Dxyn()
+{
+	uint8_t Vx = (opcodes & 0x0F00u) >> 8u;
+	uint8_t Vy = (opcodes & 0x00F0u) >> 4u;
+	uint8_t height = opcodes & 0x000Fu;
+
+	// Wrap if going beyond screen boundaries
+	uint8_t xPos = V[Vx] % 64;
+	uint8_t yPos = V[Vy] % 32;
+
+	V[0xF] = 0;
+
+	for (unsigned int row = 0; row < height; ++row)
+	{
+		uint8_t spriteByte = memory[I + row];
+
+		for (unsigned int col = 0; col < 8; ++col)
+		{
+			uint8_t spritePixel = spriteByte & (0x80u >> col);
+			uint8_t* screenPixel = &gfx[(yPos + row) * 64 + (xPos + col)];
+
+			// Sprite pixel is on
+			if (spritePixel)
+			{
+				// Screen pixel also on - collision
+				if (*screenPixel == 0xFFFFFFFF)
+				{
+					V[0xF] = 1;
+				}
+
+				// Effectively XOR with the sprite pixel
+				*screenPixel ^= 0xFFFFFFFF;
+			}
+		}
+	}
+}
+
+void printScreen(){
+    for(int j=0;j<32;j++){
+        for(int i=0;i<64;i++){
+
+            if(gfx[(64*j)+i] == 0xFF){
+               printf("█");
+            }
+            else{
+                printf(" ");
+            }
+            //printf("%d",gfx[(64*j)+i]);
+        }
+        printf("\n");
+    }
+
 }
