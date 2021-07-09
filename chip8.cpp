1c891d99fb95eb1b826bb2c1a766a26f8bcf7df5
Tushar Upadhyay <tushar.u110@gmail.com>
2021-07-09 12:26:17 +0530

    added SDL window rendering

--- chip8.cpp
+++ chip8.cpp
@@ -1,6 +1,8 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 #include <cstdint>
 #include <cstring>
+#include <ctime>
 
 using namespace std;
 
@@ -28,6 +30,10 @@ uint8_t chip8_fontset[80] = {
   0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
+
+void Update(void const* buffer, int pitch, SDL_Renderer* renderer,SDL_Texture* texture);
+bool ProcessInput(uint8_t* keys);
+
 class chip8{
 public:
 
@@ -52,7 +58,7 @@ public:
     // 1 byte x 2K Video Memory (64 x 32)
     uint32_t gfx[screen_width * screen_height];
 
-    // stack and stack pointer (To make the emulator run not in actual chip 8)
+    // stack and stack pointer 
     uint16_t stack[16];
     uint16_t sp;
 
@@ -431,16 +437,211 @@ void chip8::printScreen(){
     }
 }
 
-int main(){
+int main(int argc, char* argv[]){
     int n;
     chip8 c;
     c.loadProgram();
     uint16_t i=0;
-    for(;;){
-        c.emulateCycle();
-        if(i==0)
-            c.printScreen();
-        i++;
+    time_t lastRead = 0;
+    time_t curTime;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("CHIP 8 Emu", 0, 0, screen_width * 10, screen_height * 10, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, screen_width, screen_height); 
+
+    bool quit = false;
+
+    while(!quit){
+
+        //quit = ProcessInput(c.keypad);
+
+        curTime = time(NULL);
+        if(curTime > 1 + lastRead){
+            lastRead = curTime;
+            c.emulateCycle();
+            Update(c.gfx, sizeof(c.gfx[0])*screen_width, renderer, texture);
+        }
     }
     return 0;
+}
+
+bool ProcessInput(uint8_t* keys){
+	bool quit = false;
+
+	SDL_Event event;
+
+		while (SDL_PollEvent(&event)){}
+			switch (event.type){
+				case SDL_QUIT:
+					quit = true;
+				    break;
+				case SDL_KEYDOWN:{
+
+					switch (event.key.keysym.sym){
+						case SDLK_ESCAPE:
+							quit = true;
+						    break;
+
+						case SDLK_x:
+							keys[0] = 1;
+						    break;
+
+						case SDLK_1:
+							keys[1] = 1;
+						    break;
+
+						case SDLK_2:
+							keys[2] = 1;
+						    break;
+
+						case SDLK_3:
+							keys[3] = 1;
+						    break;
+
+						case SDLK_q:
+							keys[4] = 1;
+						    break;
+
+						case SDLK_w:
+							keys[5] = 1;
+						    break;
+
+						case SDLK_e:
+							keys[6] = 1;
+						    break;
+
+						case SDLK_a:
+							keys[7] = 1;
+						    break;
+
+						case SDLK_s:
+							keys[8] = 1;
+						    break;
+
+						case SDLK_d:
+							keys[9] = 1;
+						    break;
+
+						case SDLK_z:
+							keys[0xA] = 1;
+						    break;
+
+						case SDLK_c:
+							keys[0xB] = 1;
+						    break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 1;
+						} break;
+
+						case SDLK_r:
+							keys[0xD] = 1;
+						    break;
+
+						case SDLK_f:
+							keys[0xE] = 1;
+						    break;
+
+						case SDLK_v:
+							keys[0xF] = 1;
+						    break;
+					}
+				} break;
+
+				case SDL_KEYUP:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_x:
+						{
+							keys[0] = 0;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 0;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 0;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 0;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 0;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 0;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 0;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 0;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 0;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 0;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 0;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 0;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 0;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 0;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 0;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 0;
+						} break;
+					}
+				} break;
+			}
+            return quit;
+		}
+
+void Update(void const* buffer, int pitch, SDL_Renderer* renderer,SDL_Texture* texture){
+	SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+	SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
 }
